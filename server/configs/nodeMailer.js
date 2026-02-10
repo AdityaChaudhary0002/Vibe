d498962c52b7db68c5b9fc4c0dcd96afda18e6a9
aditya adi <adityaadi@adityas-MacBook-Air-2.local>
2026-02-10 17:45:49 +0530

    Cleanup: Remove debug logs

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -1,17 +1,6 @@
 import nodemailer from "nodemailer";
 
 // Create a transporter object using the SMTP settings
-console.log("SMTP_USER Available:", !!process.env.SMTP_USER);
-console.log("SMTP_PASS Available:", !!process.env.SMTP_PASS);
-
-if (!process.env.SMTP_USER) {
-  console.error("FATAL ERROR: SMTP_USER is undefined in process.env");
-}
-
-if (!process.env.SMTP_PASS) {
-  console.error("FATAL ERROR: SMTP_PASS is undefined in process.env");
-}
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
